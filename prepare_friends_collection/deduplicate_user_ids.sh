451f56186b98ea0f9ddb463eab853af755e5f300
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2023-07-13 18:53:42 +0200

    prepare files for last bios collection

--- prepare_friends_collection/deduplicate_user_ids.sh
+++ prepare_friends_collection/deduplicate_user_ids.sh
@@ -148,3 +148,39 @@ cd ../..
 
 # ENGLISH CHUNK 2-2 999-
 
+cd english_chunk2/less_999-part2
+mkdir tmpsort
+
+ls english_999-_friends_chunk2-leftover1+2_after_230209_part_?.csv english_999-_friends_chunk2-leftover1+2_after_230209_part_??.csv | while read SOURCE_FILE; do
+  xsv select user_id $SOURCE_FILE | xsv behead | sort -u > $SOURCE_FILE.unique_userids
+done
+cat english_999-_friends_chunk2-leftover1+2_after_230209_part_?.csv.unique_userids english_999-_friends_chunk2-leftover1+2_after_230209_part_??.csv.unique_userids | sort -u > english_999-_friends_chunk2-leftover1+2_after_230209_parts.csv.unique_userids
+
+xsv cat rows english_999-_friends_chunk2-leftover1+2_after_230209_part_*_results.csv > english_999-_friends_chunk2-leftover1+2_after_230209_results.csv
+gzip *results.csv
+
+ls english_999-_friends_chunk2-leftover1+2_after_230209_part_*_results.csv.gz | while read RESULT_FILE; do
+  zcat $RESULT_FILE | xsv select friend_id | xsv behead | sort -u > $RESULT_FILE.unique_userids
+done
+cat english_999-_friends_chunk2-leftover1+2_after_230209_part_*_results.csv.gz.unique_userids | sort -u -T tmpsort > english_999-_friends_chunk2-leftover1+2_after_230209_parts_results.csv.gz.unique_userids
+
+echo "user_id" > english_999-_friends_chunk2-leftover1+2_after_230209_unique_userids.csv
+cat english_999-_friends_chunk2-leftover1+2_after_230209_parts.csv.unique_userids english_999-_friends_chunk2-leftover1+2_after_230209_parts_results.csv.gz.unique_userids | sort -u -T tmpsort >> english_999-_friends_chunk2-leftover1+2_after_230209_unique_userids.csv
+gzip english_999-_friends_chunk2-leftover1+2_after_230209_unique_userids.csv
+
+rm -f *.unique_userids
+cd ../..
+
+
+# ASSEMBLE + ZIP
+
+cd english_chunk2
+echo "user_id" > english_chunk2_after_230209_unique_userids.csv
+cat more_1000/english_1000+_friends_chunk2_after_230209_unique_userids.csv less_999-part1/english_999-_friends_chunk2-1_after_230209_cut_unique_userids.csv less_999-part2/english_999-_friends_chunk2-leftover1+2_after_230209_unique_userids.csv | grep -v "^user_id" | sort -u -T tmpsort >> english_chunk2_after_230209_unique_userids.csv
+
+gzip */english_chunk2*_unique_userids.csv
+
+casa map 'index % 6 + 1' partition english_chunk2_after_230209_unique_userids.csv > english_chunk2_after_230209_unique_userids_+6partitions.csv
+cd ..
+xsv partition partition bios --drop --filename 'english_chunk2_after_230209_unique_userids_part_{}.csv' english_chunk2/english_chunk2_after_230209_unique_userids_+6partitions.csv
+
